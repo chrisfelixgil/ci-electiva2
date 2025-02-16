d03baedbb890adcf0ad5daead200a4063e761cb3
Chris Castle <christiangil2705@hotmail.com>
2025-02-16 10:06:59 -0400

    CI utilizando c# y Xunit.NET

---
+++ CalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using easy_calculator;
+
+public class CalculatorTest
+{
+
+    private readonly Calculator _calculator;
+
+    public CalculatorTest()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Fact]
+    public void Sumar_DosNumeros_RetornarResultadoCorrecto()
+    {
+        int resultado = _calculator.Sumar(3, 5);
+        Assert.Equal(8, resultado);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace easy_calculator
+{
+    public class Calculator 
+    {
+        public int Sumar(int a, int b) => a + b;
+        
+    }
+
+}
